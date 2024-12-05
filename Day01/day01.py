0695e5f4c918d851f52d7bfc89d551965fbd1f04
Joanne Longeville <joanne.longeville@gmail.com>
2024-12-05 11:41:32 +0100

    finished day01 part01

---
+++ Day01/day01.py
@@ -0,0 +1,44 @@
+# Part 01
+
+def import_input(file):
+    with open(file, 'r') as file:
+        input = [list(map(int, line.split())) for line in file if line.strip()]
+    return input
+
+
+def pair_up(data):
+    left = sorted([row[0] for row in data])
+    rght = sorted([row[1] for row in data])
+    paired = list(zip(left, rght))
+    return paired
+
+
+def pair_differences(pairs):
+    differences = [abs(pair[0] - pair[1]) for pair in pairs]
+    return differences
+
+
+def sum_differences(differences):
+    return sum(differences)
+
+
+def test_part_one():
+    locations = import_input('input_test.yml')
+    locations = pair_up(locations)
+    locations = pair_differences(locations)
+    locations = sum_differences(locations)
+    return locations
+
+
+print('test_part_one: ', test_part_one())
+
+
+def part_one():
+    locations = import_input('input.yml')
+    locations = pair_up(locations)
+    locations = pair_differences(locations)
+    locations = sum_differences(locations)
+    return locations
+
+
+print('part_one: ', part_one())
