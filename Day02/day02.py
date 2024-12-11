0fd57436c7fae73b80a13816b7ff29a83719e27b
Joanne Longeville <joanne.longeville@gmail.com>
2024-12-11 17:11:54 +0100

    add day02

---
+++ Day02/day02.py
@@ -0,0 +1,37 @@
+# Part 01
+
+def import_input(file):
+    with open(file, 'r') as file:
+        input = [list(map(int, line.split())) for line in file if line.strip()]
+    return input
+
+
+def test_part_one():
+    return
+
+
+print('test_part_one: ', test_part_one())
+
+
+def part_one():
+    return
+
+
+print('part_one: ', part_one())
+
+
+# Part 02
+
+
+def test_part_two():
+    return
+
+
+print('test_part_two: ', test_part_two())
+
+
+def part_two():
+    return
+
+
+print('part_two: ', part_two())
